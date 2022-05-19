ba31b26520dec4b239d94e5d602647ae86b62bb4
seung <38664481+seunggil1@users.noreply.github.com>
2022-05-20 02:30:42 +0900

    한글 인코딩 처리 로직 추가

--- src/main/java/com/ssafy/vue/VueApiApplication.java
+++ src/main/java/com/ssafy/vue/VueApiApplication.java
@@ -2,12 +2,22 @@ package com.ssafy.vue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
 
 @SpringBootApplication
-public class VueApiApplication {
+public class VueApiApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VueApiApplication.class, args);
 	}
+	
+	@Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setUrlDecode(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
 
 }
